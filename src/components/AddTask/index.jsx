c0feb6573b08b0e6c2da84bc29ec2b757901a5cd
rogdli <rogdli.2000@gmail.com>
2024-09-10 19:33:06 -0300

    Actualización de nombre de función handleAddTask para mejor legibilidad.

--- src/components/AddTask/index.jsx
+++ src/components/AddTask/index.jsx
@@ -8,29 +8,29 @@ function AddTask({ onAddTask }) {
     setNewTask(event.target.value);
   }
 
-  function addTask() {
+  function handleAddTask() {
     if (newTask.trim() !== "") {
-      onAddTask(newTask);
-      setNewTask("");
+      onAddTask(newTask); // Llama a la función pasada desde el componente padre (ToDoList.jsx).
+      setNewTask(""); // Limpia el campo de texto.
     }
   }
 
   return (
     <div>
-      <h2 className = "title">Lista de tareas de Rodrigo</h2>
+      <h2 className="title">Lista de tareas de Rodrigo</h2>
       <input 
-        className = "input-box"
+        className="input-box"
         type="text"
         placeholder="Agregar tarea..."
         value={newTask}
         onChange={handleInputChange}
       />
-      <button className = 'add-button' 
-              onClick={addTask}>
-                Add
+      <button className='add-button' onClick={handleAddTask}>
+        Add
       </button>
     </div>
   );
 }
 
 export default AddTask;
+
